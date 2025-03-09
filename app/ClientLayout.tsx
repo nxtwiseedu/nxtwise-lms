5a284f62e5c6d7abbd1fa0f7833fbeb7116062b2
Gururajj77 <gururajtakeshi@gmail.com>
2025-03-09 13:59:58 +0530

    fidxed dashboard

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -6,7 +6,7 @@ import { usePathname } from "next/navigation";
 
 export function ClientLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-  const isLoginPage = pathname === "/login" || pathname === "/signup";
+  const isLoginPage = pathname === "/login";
 
   // If it's the login page, don't wrap with NavBar
   if (isLoginPage) {
